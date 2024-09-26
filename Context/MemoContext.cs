4b1d39383700a5fa3586c414f1a4f5364fd58e48
MuyuMyu <muyu28372@gmail.com>
2024-09-26 21:41:40 +0800

    first commit

---
+++ Context/MemoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Memo.api.Context
+{
+    public class MemoContext : DbContext
+    {
+
+        public MemoContext(DbContextOptions<MemoContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<ToDo> ToDo { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Memo> Memo { get; set; }
+
+    }
+}
